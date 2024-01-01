61ae35fb01de0823373522e2eb61785be2d20d06
Pasinee <pasinee.sb@gmail.com>
2024-01-01 09:27:25 -0500

    update dependency

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const BASE_URL =
-  // "https://pharmamate-backend.onrender.com";
-  // ||
-  "http://localhost:3001";
+const BASE_URL = "https://pharmamate-backend.onrender.com";
+// ||
+// "http://localhost:3001";
 
 /** API Class.
  *
